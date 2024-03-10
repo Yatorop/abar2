045a45654e4a446571f1c47e64cd6618cb4ffec9
Yatorop <162882173+Yatorop@users.noreply.github.com>
2024-03-10 13:10:30 +0200

    Add files via upload

---
+++ menu_window.py
@@ -0,0 +1,65 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit,\
+       QHBoxLayout, QVBoxLayout, QPushButton, QLabel
+
+
+menu_win = QWidget()
+
+lb_quest = QLabel('Введіть запитання:')
+lb_quest.setStyleSheet("background-color: yellow;")
+lb_right_ans = QLabel('Введіть вірну відповідь:')
+lb_right_ans.setStyleSheet("background-color: purple;")
+lb_wrong_ans1 = QLabel('Введіть першу хибну відповідь')
+lb_wrong_ans1.setStyleSheet("background-color: green;")
+lb_wrong_ans2 = QLabel('Введіть другу хибну відповідь')
+lb_wrong_ans2.setStyleSheet("background-color: pink;")
+lb_wrong_ans3 = QLabel('Введіть третю хибну відповідь')
+lb_wrong_ans3.setStyleSheet("background-color: purple;")
+
+le_question = QLineEdit()
+le_right_ans = QLineEdit()
+le_wrong_ans1 = QLineEdit()
+le_wrong_ans2 = QLineEdit()
+le_wrong_ans3 = QLineEdit()
+
+lb_header_stat = QLabel('Статистика')
+lb_header_stat.setStyleSheet('font-size: 19px; font-weight: bold;')
+
+lb_statistic = QLabel()
+
+vl_labels = QVBoxLayout()
+vl_labels.addWidget(lb_quest)
+vl_labels.addWidget(lb_right_ans)
+vl_labels.addWidget(lb_wrong_ans1)
+vl_labels.addWidget(lb_wrong_ans2)
+vl_labels.addWidget(lb_wrong_ans3)
+
+vl_lineEdits = QVBoxLayout()
+vl_lineEdits.addWidget(le_question)
+vl_lineEdits.addWidget(le_right_ans)
+vl_lineEdits.addWidget(le_wrong_ans1)
+vl_lineEdits.addWidget(le_wrong_ans2)
+vl_lineEdits.addWidget(le_wrong_ans3)
+
+hl_question = QHBoxLayout()
+hl_question.addLayout(vl_labels)
+hl_question.addLayout(vl_lineEdits)
+
+btn_back = QPushButton('Назад')
+btn_back.setStyleSheet("background-color: yellow;")
+btn_add_question = QPushButton('Додати запитання')
+btn_add_question.setStyleSheet("background-color: green;")
+btn_clear = QPushButton('Очистити')
+btn_clear.setStyleSheet("background:rgb(0,0,255);")
+hl_buttons = QHBoxLayout()
+hl_buttons.addWidget(btn_add_question)
+hl_buttons.addWidget(btn_clear)
+
+vl_res = QVBoxLayout()
+vl_res.addLayout(hl_question)
+vl_res.addLayout(hl_buttons)
+vl_res.addWidget(lb_header_stat)
+vl_res.addWidget(lb_statistic)
+vl_res.addWidget(btn_back)
+
+menu_win.setLayout(vl_res)
+menu_win.resize(400, 300)
